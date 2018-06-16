2a030f52b3f81bd11253fb3e83585a4b0d88b759
Martin Fetzel <martin.fetzel@get-the-solution.net>
2018-06-16 21:35:31 +0200

    #31 moved generatefilename to imagefilebase

---
+++ Get.the.solution.Image.Manipulation.ViewModel/Service/ImageFileBaseService.cs
@@ -0,0 +1,51 @@
+﻿using Get.the.solution.Image.Manipulation.Contract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Get.the.solution.Image.Manipulation.ViewModel.Service
+{
+    public abstract class ImageFileBaseService
+    {
+        protected ILoggerService _loggerService;
+        public ImageFileBaseService(ILoggerService loggerService)
+        {
+            _loggerService = loggerService;
+        }
+        public string GenerateResizedFileName(ImageFile storeage, int? width, int? height)
+        {
+            try
+            {
+                if (!width.HasValue)
+                {
+                    width = storeage.Width;
+                }
+                if (!height.HasValue)
+                {
+                    height = storeage.Height;
+                }
+                if (storeage != null)
+                {
+                    string suggestedfilename = $"{storeage.FileInfo.Name.Replace(storeage.FileInfo.Extension, String.Empty)}-{width}x{height}{storeage.FileInfo.Extension}";
+                    //String suggestedfilename = $"{storeage.Name.Replace(storeage.FileType, String.Empty)}-{Width}x{Height}{storeage.FileType}";
+                    _loggerService?.LogEvent(nameof(GenerateResizedFileName), new Dictionary<String, String>()
+                        {
+                            { nameof(suggestedfilename), $"{suggestedfilename}" },
+                            { nameof(FileInfo.Extension), $"{new FileInfo(suggestedfilename)?.Extension}" },
+                        });
+                    return suggestedfilename;
+                }
+                else
+                {
+                    return String.Empty;
+                }
+            }
+            catch (Exception e)
+            {
+                _loggerService?.LogException(nameof(GenerateResizedFileName), e);
+            }
+            return String.Empty;
+        }
+    }
+}
