1ecbd72373764d450fa9a837e19a135d7e22fbba
Martin Fetzel <martin.fetzel@get-the-solution.net>
2018-08-21 21:03:06 +0200

    Removed UWP Test project

---
+++ Get.the.solution.Image.Azure.Cognitive.Services/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Get.the.solution.Image.Azure.Cognitive.Services
+{
+    public class CognitiveServices
+    {
+        public Task AnalyzeanImage()
+        {
+
+            return Task.CompletedTask;
+        }
+    }
+}
