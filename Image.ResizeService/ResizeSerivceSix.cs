60f8a16c2a56c6939185525f26d7ad20bce9fb00
Martin Fetzel <martin.fetzel@get-the-solution.net>
2020-08-26 22:08:42 +0200

    Use image.Save Method and set format manual by file extension

--- Image.ResizeService/ResizeSerivceSix.cs
+++ Image.ResizeService/ResizeSerivceSix.cs
@@ -12,17 +12,9 @@ namespace Get.the.solution.Image.Manipulation.ResizeService
     public class ResizeSerivceSix : IResizeService
     {
         protected readonly ILoggerService _loggerService;
-        protected readonly Configuration _configuration;
         public ResizeSerivceSix(ILoggerService loggerService)
         {
             _loggerService = loggerService;
-            //for some reason DetectFormat returns tga for images - so we use our own configuration
-            _configuration = new Configuration(
-                  new SixLabors.ImageSharp.Formats.Bmp.BmpConfigurationModule()
-                , new SixLabors.ImageSharp.Formats.Gif.GifConfigurationModule()
-                , new SixLabors.ImageSharp.Formats.Png.PngConfigurationModule()
-                , new SixLabors.ImageSharp.Formats.Jpeg.JpegConfigurationModule()
-                );
         }
         public MemoryStream Resize(Stream inputStream, int width, int height, string suggestedFileName = null, int quality = 75)
         {
@@ -32,51 +24,53 @@ namespace Get.the.solution.Image.Manipulation.ResizeService
             }
             try
             {
-                using (var image = SixLabors.ImageSharp.Image.Load(_configuration, inputStream))
+                using (var image = SixLabors.ImageSharp.Image.Load(inputStream))
                 {
-                    var format = SixLabors.ImageSharp.Image.DetectFormat(_configuration, inputStream);
-
                     var output = new MemoryStream();
                     image.Mutate((x) => x.AutoOrient().Resize(width, height));
+
+                    string extension = new FileInfo(suggestedFileName).Extension.ToLowerInvariant();
+                    var format = SixLabors.ImageSharp.Image.DetectFormat(inputStream);
                     if (format == null)
                     {
-                        string extension = new FileInfo(suggestedFileName).Extension.ToLowerInvariant();
                         _loggerService.LogEvent(
                             $"{nameof(SixLabors.ImageSharp.Image.DetectFormat)} is null. Using image extension", extension);
                         if (".gif".Equals(extension))
                         {
-                            image.SaveAsGif(output);
+                            format = SixLabors.ImageSharp.Formats.Gif.GifFormat.Instance;
                         }
                         else if (".bmp".Equals(extension))
                         {
-                            image.SaveAsBmp(output);
+                            format = SixLabors.ImageSharp.Formats.Bmp.BmpFormat.Instance;
                         }
                         else if (".png".Equals(extension))
                         {
-                            image.SaveAsPng(output);
+                            format = SixLabors.ImageSharp.Formats.Png.PngFormat.Instance;
                         }
                         else
                         {
-                            if (quality != 100)
-                            {
-                                image.SaveAsJpeg(output, new SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder() { Quality = quality });
-                            }
-                            else
-                            {
-                                image.SaveAsJpeg(output);
-                            }
+                            format = SixLabors.ImageSharp.Formats.Jpeg.JpegFormat.Instance;
                         }
+                    }
+
+                    _loggerService.LogEvent(
+                        $"{nameof(SixLabors.ImageSharp.Image.DetectFormat)} is", format?.Name);
+
+                    if(format is SixLabors.ImageSharp.Formats.Jpeg.JpegFormat)
+                    {
+                        image.Save(output,
+                            new SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder() { Quality = quality });
                     }
                     else
                     {
-                        _loggerService.LogEvent(
-                            $"{nameof(SixLabors.ImageSharp.Image.DetectFormat)} is", format?.Name);
                         image.Save(output, format);
                     }
+
+
                     return output;
                 }
             }
-            catch(UnknownImageFormatException ui)
+            catch (UnknownImageFormatException ui)
             {
                 throw new Image.Contract.Exceptions.UnknownImageFormatException(ui.Message, ui);
             }
